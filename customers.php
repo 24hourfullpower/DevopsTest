57884b5d3fa42cb5062c0676e69f3d7bc7353af2
24hourfullpower <cs.mayankyadav@gmail.com>
2016-07-31 16:08:26 +0530

    Create customers.php

---
+++ customers.php
@@ -0,0 +1,10 @@
+<?php
+
+function get_data() {
+        
+   $dbhost = 'localhost:3036';
+   $dbuser = 'root';
+   $dbpass = 'rootpassword';
+   
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+?>
