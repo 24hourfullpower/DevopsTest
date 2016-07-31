bae9dbe4ae31f6272b25097a6ecf36b0f39f3c60
24hourfullpower <cs.mayankyadav@gmail.com>
2016-07-31 16:11:34 +0530

    Update customers.php

--- customers.php
+++ customers.php
@@ -4,7 +4,30 @@ function get_data() {
         
    $dbhost = 'localhost:3036';
    $dbuser = 'root';
-   $dbpass = 'rootpassword';
+   $dbpass = 'root';
    
    $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
+   
+   $sql = 'SELECT customer_name, customer_age FROM customer';
+   mysql_select_db('aon_db');
+   $output = mysql_query( $sql, $conn );
+   
+   if(! $retval ) {
+      die('Could not get data: ' . mysql_error());
+   }
+   
+   while($row = mysql_fetch_array($output, MYSQL_ASSOC)) {
+      echo "Customer Name :{$row['customer_name']}  <br> ".
+         "Customer Age : {$row['customer_age']} <br> ".
+         "--------------------------------<br>";
+   }
+   
+   echo "Fetched data successfully";
+   
+   mysql_close($conn);
+}
 ?>
